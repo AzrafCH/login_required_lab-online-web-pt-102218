42c9c35259d065ebce0dfa95ad6aaeebb888664d
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-26 00:25:10 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,6 @@
 class SessionsController < ApplicationController
-  def show
-    return head(:forbidden) unless session.include? :user_id
-    @session = Session.find(params[:id])
-  end
+def new
+end
 
-  def index
-  end
-
-  def create
-  end
-
-  def update
-  end
 
 end
