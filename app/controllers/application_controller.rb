f3948c7d18b94e542e09b7a187ce3ab4ee707404
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-26 00:31:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def hello
     redirect_to controller: 'sessions', action: 'new' unless current_user
   end
+
+  def current_user
+    session[:name]
+  end 
+
+  def require_logged_in
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+  end 
 end
